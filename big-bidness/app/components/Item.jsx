481abbf55dca7448103cfbab68f2d0d83a25644d
Buringskul <simonaisakova6@gmail.com>
2024-11-17 18:03:59 -0500

    Updated card display

--- big-bidness/app/components/Item.jsx
+++ big-bidness/app/components/Item.jsx
@@ -1,26 +1,34 @@
 import React from 'react'
+import { Card, CardHeader, CardBody, CardFooter, Typography, Button } from '@material-tailwind/react';
 
-export const Item = ({data}) => {
+export const Item = ({ data }) => {
   return (
-    <div className='w-11/12 block border-black border-2'>
-      <div className='justify-items-center pt-2'>
-        <img className='w-full px-2 h-full'
-          src={data.image}
-          alt="Image" />
-      </div>
-      <div>
-        <h1>
-          {data.name}
-        </h1>
-        <p>
-          {data.price}
-        </p>
-      </div>
-      <div>
-        <button>
-          Buy Now
-        </button>
-      </div>
-    </div>
-  )
+    <a href={data.productUrl} className="block cursor-pointer">
+      <Card className="mt-6 w-90 m-1 border border-gray-300 rounded-lg shadow-md">
+        <CardHeader color="blue-gray" className="relative h-56">
+          <img 
+            src={data.image} 
+            alt="Product" 
+            className="w-full h-full object-cover rounded-t-lg" 
+          />
+        </CardHeader>
+        <CardBody className="p-4">
+          <Typography variant="h5" color="blue-gray" className="mb-2">
+            {data.name}
+          </Typography>
+          <Typography color="gray" className="mb-2">
+            {data.price}
+          </Typography>
+          <Typography color="gray" className="mb-2">
+            {data.description}
+          </Typography>
+        </CardBody>
+        <CardFooter className="pt-0">
+          <Button className="bg-black text-white hover:bg-gray-800 py-2 px-4 text-lg rounded-lg">
+            Buy Now
+          </Button>
+        </CardFooter>
+      </Card>
+    </a>
+  );
 }
